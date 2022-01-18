0a91464b6ad428d2c6ec06599c21158fa39a82c5
Brayam Valqui <bd.valqui@gmail.com>
2022-01-18 12:45:00 -0500

    Reaction Function files

--- test/runtests_EPEC_Reaction.jl
+++ test/runtests_EPEC_Reaction.jl
@@ -13,6 +13,19 @@ println("*****************EPEC Solution*****************")
 Number_Runs=20
 equilibrium_investments=zeros(7,Number_Runs)
 
+set_wind_options_par=[13;14;15;16;17;18;19;20]
+set_thermalgenerators_options_par=[25;26;27;28;29;30;31;32;33;34;35;36;37;38;39;40;41;42;43;44]
+n_Gas_CT=1:4
+set_Gac_CT=[27;32;37;42]
+index_Gas_CT=[1;2;1;2]
+
+n_Gas_CC_CCS=1:4
+set_Gac_CC_CCS=[29;34;39;44]
+index_Gas_CC_CCS=[1;2;1;2]
+
+n_Solar=1:4
+set_Solar=[14,16,18,20]
+index_Solar=[1;2;1;2]
 #========================================================================================================================#
 #Firm A's decisions are variables. Firm B's decisions are parameters (Leader=1)
 
@@ -22,7 +35,25 @@ start_point=1
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+
+#=
+for i in n_Gas_CT if index_Gas_CT[i]==Leader
+deleteat!(set_thermalgenerators_options_par, findall(x->x==set_Gac_CT[i],set_thermalgenerators_options_par))
+end
+end
+
+
+for i in n_Gas_CC_CCS if index_Gas_CC_CCS[i]==Leader
+deleteat!(set_thermalgenerators_options_par, findall(x->x==set_Gac_CC_CCS[i],set_thermalgenerators_options_par))
+end
+end
+=#
+for i in n_Solar if index_Solar[i]==Leader
+deleteat!(set_wind_options_par, findall(x->x==set_Solar[i],set_wind_options_par))
+end
+end
+
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -32,7 +63,8 @@ start_point=2
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -43,7 +75,7 @@ start_point=3
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -53,7 +85,7 @@ start_point=4
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -63,7 +95,7 @@ start_point=5
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -73,7 +105,7 @@ start_point=6
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -83,7 +115,7 @@ start_point=7
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -93,7 +125,7 @@ start_point=8
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -103,7 +135,7 @@ start_point=9
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -113,7 +145,7 @@ start_point=10
 
 Leader=1
 Follower=2
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 #========================================================================================================================#
@@ -121,13 +153,34 @@ equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 #========================================================================================================================#
 #Firm B's decisions are variables. Firm A's decisions are parameters (Leader=2)
 
+set_wind_options_par=[13;14;15;16;17;18;19;20]
+set_thermalgenerators_options_par=[25;26;27;28;29;30;31;32;33;34;35;36;37;38;39;40;41;42;43;44]
+
 start_point=11
 
 (x_w_p,x_e_p)=loadinputs_Reaction_Investments("data_reaction_function/$start_point")
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+
+#=
+for i in n_Gas_CT if index_Gas_CT[i]==Leader
+deleteat!(set_thermalgenerators_options_par, findall(x->x==set_Gac_CT[i],set_thermalgenerators_options_par))
+end
+end
+=#
+
+for i in n_Gas_CC_CCS if index_Gas_CC_CCS[i]==Leader
+deleteat!(set_thermalgenerators_options_par, findall(x->x==set_Gac_CC_CCS[i],set_thermalgenerators_options_par))
+end
+end
+
+for i in n_Solar if index_Solar[i]==Leader
+deleteat!(set_wind_options_par, findall(x->x==set_Solar[i],set_wind_options_par))
+end
+end
+
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -137,7 +190,7 @@ start_point=12
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -147,7 +200,7 @@ start_point=13
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -157,7 +210,7 @@ start_point=14
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -167,7 +220,7 @@ start_point=15
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -178,7 +231,7 @@ start_point=16
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -189,7 +242,7 @@ start_point=17
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -200,7 +253,7 @@ start_point=18
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -211,7 +264,7 @@ start_point=19
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
@@ -222,12 +275,12 @@ start_point=20
 
 Leader=2
 Follower=1
-@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
+@time (profit_det_MPEC,x_w_value_MPEC,x_e_value_MPEC,Total_Investments_Technology_EPEC_par,Total_Investments_Technology_Firms_EPEC_par)=Solver_EPEC_Reaction(x_w_p,x_e_p,start_point,Leader,Follower,set_thermalgenerators_options_par,set_wind_options_par,Gurobi.Optimizer,"results/EPEC_Solution.txt","results_startingpoints/$start_point")
 
 equilibrium_investments[:,start_point]=Total_Investments_Technology_EPEC_par
 
 
-(equilibrium_investments_sol)=Plots_CapacityMix_All_Solutions(equilibrium_investments,Number_Runs,"results_startingpoints")
+#(equilibrium_investments_sol)=Plots_CapacityMix_All_Solutions(equilibrium_investments,Number_Runs,Profits_Objective_Function,"results_startingpoints")
 
 
 
