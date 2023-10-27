15af8d732a8a791b0d4dadcc650a73704fa70aff
pbva002 <bvalqui@epri.com>
2023-10-26 21:59:28 -0400

    Including explanation to run the model in runtests_EPEC_StartingPoints_Nfirms.jl

--- src/src_PJM/loadinputs_Reaction_Investments.jl
+++
@@ -1,8 +0,0 @@
-function loadinputs_Reaction_Investments(folder::String)
-
-    #Congestion
-    x_w_p=CSV.File(folder *"/wind_investments.csv") |> Tables.matrix
-    x_e_p=CSV.File(folder *"/thermal_investments.csv") |> Tables.matrix
-
-return (x_w_p,x_e_p)
-end
